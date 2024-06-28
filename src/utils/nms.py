51d6370d04ca86316b33c14e7545fcae464bcc5e
chuvalniy <chuvalik.work@gmail.com>
2024-06-28 08:55:12 +0300

    implement nms

--- src/utils/nms.py
+++ src/utils/nms.py
@@ -10,17 +10,32 @@ def non_maximum_suppression(x, iou_threshold=0.5):
     :param iou_threshold: IoU threshold for suppressing bounding box
     """
 
-    sorted_tensor, sorted_indices = torch.sort(x[:, 0], descending=True)
-    sorted_x = x[sorted_indices]
+    boxes = sorted(x, key=lambda i: i[1], reverse=True)
 
+    nms_boxes = []
+    while boxes:
+        chosen_box = boxes.pop(0)
 
+        boxes = [
+            box
+            for box in boxes
+            if box[0] != chosen_box[0]
+            or
+            intersection_over_union(
+                chosen_box[2:], box[2:]
+            ) < iou_threshold
+        ]
+
+        nms_boxes.append(chosen_box)
+
+    return nms_boxes
 
 
 if __name__ == '__main__':
     x = torch.tensor([
-        [0.5, 0.75, 0.69, 0.1, 0.1],
-        [0.7, 0.2, 0.25, 0.1, 0.11],
-        [0.2, 0.2, 0.25, 0.21, 0.13]
+        [1, 0.5, 0.1, 0.1, 0.3, 0.3],
+        [1, 0.7, 0.1, 0.11, 0.3, 0.31],
+        [0, 0.2, 0.2, 0.25, 0.21, 0.13]
     ])
 
     bboxes = non_maximum_suppression(x)
